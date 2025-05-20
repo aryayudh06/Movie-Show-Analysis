ab9508a6cf5f23bb0d00d77f6b0443b39f0dfdc1
Aimarr03 <andyaimar729@gmail.com>
2025-05-20 21:13:51 +0700

    Adding Trakt.tv extraction

--- extract.py
+++ extract.py
@@ -8,19 +8,20 @@ class ExtractData():
     pass
     # load_dotenv()  # Secara default mencari file .env di direktori saat ini
 
-  def fetch_tmdb_movies(api_key, max_items=50):
+  def fetch_tmdb_movies(self, api_key, max_items=50):
       movies = []
       page = 1
       while len(movies) < max_items:
           url = f"https://api.themoviedb.org/3/movie/popular?api_key={api_key}&language=en-US&page={page}"
           res = requests.get(url).json()
+          print(res)
           movies.extend(res['results'])
           if page >= res['total_pages'] or len(movies) >= max_items:
               break
           page += 1
       return movies[:max_items]
 
-  def fetch_tvmaze_shows(max_items=50):
+  def fetch_tvmaze_shows(self, max_items=50):
       shows = []
       page = 0
       while len(shows) < max_items:
@@ -36,7 +37,14 @@ class ExtractData():
 
 if __name__== "__main__":
   extract = ExtractData()
+
+  load_dotenv()
   API_KEY = os.getenv("TMDB_API")
+
   # Contoh penggunaan
-  tmdb_data = extract.fetch_tmdb_movies(API_KEY, max_items=50)
+  
+  #tmdb_data = extract.fetch_tmdb_movies(API_KEY, max_items=50)
+  #print("TMDB:", tmdb_data[0])
+  
   tvmaze_data = extract.fetch_tvmaze_shows(max_items=50)
+  print("TVMaze:", tvmaze_data[0])
